7e3fb1d3083e926f85df9e72d712d62062e764c4
arizona1danielle-commits <arizona1.danielle@gmail.com>
2026-08-29 17:10:21 -0300

    Create Carta 2.c

---
+++ Carta 2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    
+    char estado;
+    char codigo[4];
+    char cidade[50];
+    int Populucao = 12325000;
+    float Area = 1521.10f;
+    float PIB =  699.28f;
+    int Numero_de_pontos_turisticos = 50;
+
+    printf("Digite a populacao:\n");
+    scanf("%d",&Populucao);
+
+    printf("Digite a area:\n");
+    scanf("%f",&Area);
+
+    printf("Informe o Pib:\n");
+    scanf("%f", &PIB);
+
+    printf("Informe o estado:\n");
+    scanf(" %c", &estado);
+
+    printf("Informe o codigo:\n");
+    scanf("%s", codigo);
+
+    printf("Pontos turisticos:\n");
+    scanf("%d", &Numero_de_pontos_turisticos);
+
+    printf("Informe a cidade:\n");
+    getchar();
+    fgets(cidade, 50, stdin);
+
+    
+
+
+
+
+
+
+}
